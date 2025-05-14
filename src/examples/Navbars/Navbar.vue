f2d5ea1304f0844a0d7da924a29ff38274d4ae87
Vinícius Lima <vinelima@hotmail.com>
2025-05-14 02:06:18 -0300

    primeiras alterações 0.1

--- src/examples/Navbars/Navbar.vue
+++ src/examples/Navbars/Navbar.vue
@@ -1,30 +1,37 @@
 <script setup>
-import { computed, ref } from "vue";
+import { computed,  } from "vue";
 import { useStore } from "vuex";
 import { useRoute } from "vue-router";
-import Breadcrumbs from "../Breadcrumbs.vue";
+// import Breadcrumbs from "../Breadcrumbs.vue";
 
-const showMenu = ref(false);
+// const showMenu = ref(false);
 const store = useStore();
 const isRTL = computed(() => store.state.isRTL);
 
-const route = useRoute();
+// const route = useRoute();
 
-const currentRouteName = computed(() => {
-  return route.name;
-});
-const currentDirectory = computed(() => {
-  let dir = route.path.split("/")[1];
-  return dir.charAt(0).toUpperCase() + dir.slice(1);
-});
+// const currentRouteName = computed(() => {
+//   return route.name;
+// });
+// const currentDirectory = computed(() => {
+//   let dir = route.path.split("/")[1];
+//   return dir.charAt(0).toUpperCase() + dir.slice(1);
+// });
 
 const minimizeSidebar = () => store.commit("sidebarMinimize");
-const toggleConfigurator = () => store.commit("toggleConfigurator");
+// const toggleConfigurator = () => store.commit("toggleConfigurator");
 
-const closeMenu = () => {
-  setTimeout(() => {
-    showMenu.value = false;
-  }, 100);
+// const closeMenu = () => {
+//   setTimeout(() => {
+//     showMenu.value = false;
+//   }, 100);
+// };
+
+import SidenavItem from "@/examples/Sidenav/SidenavItem.vue";
+const getRoute = () => {
+  const route = useRoute();
+  const routeArr = route.path.split("/");
+  return routeArr[1];
 };
 </script>
 <template>
@@ -36,10 +43,10 @@ const closeMenu = () => {
     data-scroll="true"
   >
     <div class="px-3 py-1 container-fluid">
-      <breadcrumbs
+      <!-- <breadcrumbs
         :current-page="currentRouteName"
         :current-directory="currentDirectory"
-      />
+      /> -->
 
       <div
         class="mt-2 collapse navbar-collapse mt-sm-0 me-md-0 me-sm-4"
@@ -50,7 +57,7 @@ const closeMenu = () => {
           class="pe-md-3 d-flex align-items-center"
           :class="isRTL ? 'me-md-auto' : 'ms-md-auto'"
         >
-          <div class="input-group">
+          <!-- <div class="input-group">
             <span class="input-group-text text-body">
               <i class="fas fa-search" aria-hidden="true"></i>
             </span>
@@ -59,10 +66,33 @@ const closeMenu = () => {
               class="form-control"
               :placeholder="isRTL ? 'أكتب هنا...' : 'Type here...'"
             />
-          </div>
+          </div> -->
         </div>
         <ul class="navbar-nav justify-content-end">
-          <li class="nav-item d-flex align-items-center">
+           <li class="nav-item d-flex align-items-center">
+
+
+            <sidenav-item
+              :to="{ name: 'Bercario' }"
+              class="px-0 nav-link font-weight-bold text-white"
+              
+            >
+              <i class="fa fa-user" :class="isRTL ? 'ms-sm-2' : 'me-sm-2'"></i>
+              <span >Berçário</span>
+            </sidenav-item>
+          </li>
+          <li>
+            <sidenav-item
+          to="/fundamental"
+          :class="getRoute() === 'billing' ? 'active' : ''"
+          :navText="isRTL ? 'الفواتیر' : 'Billing'"
+        >
+          <template v-slot:icon>
+            <i class="ni ni-credit-card text-success text-sm opacity-10"></i>
+          </template>
+        </sidenav-item>
+          </li>
+          <!-- <li class="nav-item d-flex align-items-center">
             <router-link
               :to="{ name: 'Signin' }"
               class="px-0 nav-link font-weight-bold text-white"
@@ -72,7 +102,7 @@ const closeMenu = () => {
               <span v-if="isRTL" class="d-sm-inline d-none">يسجل دخول</span>
               <span v-else class="d-sm-inline d-none">Sign In</span>
             </router-link>
-          </li>
+          </li> -->
           <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
             <a
               href="#"
@@ -87,12 +117,12 @@ const closeMenu = () => {
               </div>
             </a>
           </li>
-          <li class="px-3 nav-item d-flex align-items-center">
+          <!-- <li class="px-3 nav-item d-flex align-items-center">
             <a class="p-0 nav-link text-white" @click="toggleConfigurator">
               <i class="cursor-pointer fa fa-cog fixed-plugin-button-nav"></i>
             </a>
-          </li>
-          <li
+          </li> -->
+          <!-- <li
             class="nav-item dropdown d-flex align-items-center"
             :class="isRTL ? 'ps-2' : 'pe-2'"
           >
@@ -215,7 +245,7 @@ const closeMenu = () => {
                 </a>
               </li>
             </ul>
-          </li>
+          </li> -->
         </ul>
       </div>
     </div>
--- src/views/Fundamental.vue
+++ src/views/Fundamental.vue
@@ -1,8 +1,18 @@
 <template>
-    <div>
-        <h1>Fundamental</h1>
-        <p>Esta é a página fundamental.</p>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                <h5 class="mb-0">Fundamental</h5>
+            </div>
+            <div class="card-body">
+                <p class="text-sm">Esta é a página fundamental.</p>
+            </div>
         </div>
+      </div>
+    </div>
+    </div>
 </template>
 
 <script>
