35f1f9670a8653cbefd47c0fc7a209cd7a1f4b20
Vinícius Lima <vinelima@hotmail.com>
2025-05-14 01:40:53 -0300

    depois de trocar

--- src/main.js
+++ src/main.js
@@ -1,25 +1,13 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import App from "./App.vue";
+import store from "./store";
+import router from "./router";
+import "./assets/css/nucleo-icons.css";
+import "./assets/css/nucleo-svg.css";
+import ArgonDashboard from "./argon-dashboard";
 
-import App from './App.vue'
-import router from './router'
-import CoreuiVue from '@coreui/vue'
-import CIcon from '@coreui/icons-vue'
-import { iconsSet as icons } from '@/assets/icons'
-import DocsComponents from '@/components/DocsComponents'
-import DocsExample from '@/components/DocsExample'
-import DocsIcons from '@/components/DocsIcons'
-import '@/assets/tailwind.css'
-import '@/styles/style.scss'
-
-const app = createApp(App)
-app.use(createPinia())
-app.use(router)
-app.use(CoreuiVue)
-app.provide('icons', icons)
-app.component('CIcon', CIcon)
-app.component('DocsComponents', DocsComponents)
-app.component('DocsExample', DocsExample)
-app.component('DocsIcons', DocsIcons)
-
-app.mount('#app')
+const appInstance = createApp(App);
+appInstance.use(store);
+appInstance.use(router);
+appInstance.use(ArgonDashboard);
+appInstance.mount("#app");
--- src/router/index.js
+++ src/router/index.js
@@ -1,119 +1,65 @@
-import { h, resolveComponent } from 'vue'
-import { createRouter, createWebHashHistory } from 'vue-router'
-
-import DefaultLayout from '@/layouts/DefaultLayout'
-import Bercario from '@/views/paginas/Bercario.vue'
-import Contato from '@/views/paginas/Contato.vue'
-import Depoimentos from '@/views/paginas/Depoimentos.vue'
-import Diferenciais from '@/views/paginas/Diferenciais.vue'
-import Espaco from '@/views/paginas/Espaco.vue'
-import Eventos from '@/views/paginas/Eventos.vue'
-import Fundamental from '@/views/paginas/Fundamental.vue'
-import Historia from '@/views/paginas/Historia.vue'
-import Infantil from '@/views/paginas/Infantil.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Dashboard from "../views/Dashboard.vue";
+import Tables from "../views/Tables.vue";
+import Billing from "../views/Billing.vue";
+import VirtualReality from "../views/VirtualReality.vue";
+import RTL from "../views/Rtl.vue";
+import Profile from "../views/Profile.vue";
+import Signup from "../views/Signup.vue";
+import Signin from "../views/Signin.vue";
 
 const routes = [
   {
-    path: '/',
-    component: DefaultLayout,
-    children: [
-      {
-        path: '',
-        redirect: '/dashboard'
-      },
-      {
-        path: 'dashboard',
-        name: 'Dashboard',
-        component: () => import('@/views/dashboard/Dashboard.vue'),
-      },
-      {
-        path: 'bercario',
-        name: 'Bercario',
-        component: Bercario,
-      },
-      {
-        path: 'contato',
-        name: 'Contato',
-        component: Contato,
-      },
-      {
-        path: 'depoimentos',
-        name: 'Depoimentos',
-        component: Depoimentos,
-      },
-      {
-        path: 'diferenciais',
-        name: 'Diferenciais',
-        component: Diferenciais,
-      },
-      {
-        path: 'espaco',
-        name: 'Espaco',
-        component: Espaco,
-      },
-      {
-        path: 'eventos',
-        name: 'Eventos',
-        component: Eventos,
-      },
-      {
-        path: 'fundamental',
-        name: 'Fundamental',
-        component: Fundamental,
-      },
-      {
-        path: 'historia',
-        name: 'Historia',
-        component: Historia,
-      },
-      {
-        path: 'infantil',
-        name: 'Infantil',
-        component: Infantil,
-      },
-    ],
+    path: "/",
+    name: "/",
+    redirect: "/dashboard-default",
   },
   {
-    path: '/pages',
-    redirect: '/pages/404',
-    name: 'Pages',
-    component: {
-      render() {
-        return h(resolveComponent('router-view'))
-      },
-    },
-    children: [
-      {
-        path: '404',
-        name: 'Page404',
-        component: () => import('@/views/pages/Page404'),
-      },
-      {
-        path: '500',
-        name: 'Page500',
-        component: () => import('@/views/pages/Page500'),
-      },
-      {
-        path: 'login',
-        name: 'Login',
-        component: () => import('@/views/pages/Login'),
-      },
-      {
-        path: 'register',
-        name: 'Register',
-        component: () => import('@/views/pages/Register'),
-      },
-    ],
+    path: "/dashboard-default",
+    name: "Dashboard",
+    component: Dashboard,
   },
-]
-
+  {
+    path: "/tables",
+    name: "Tables",
+    component: Tables,
+  },
+  {
+    path: "/billing",
+    name: "Billing",
+    component: Billing,
+  },
+  {
+    path: "/virtual-reality",
+    name: "Virtual Reality",
+    component: VirtualReality,
+  },
+  {
+    path: "/rtl-page",
+    name: "RTL",
+    component: RTL,
+  },
+  {
+    path: "/profile",
+    name: "Profile",
+    component: Profile,
+  },
+  {
+    path: "/signin",
+    name: "Signin",
+    component: Signin,
+  },
+  {
+    path: "/signup",
+    name: "Signup",
+    component: Signup,
+  },
+];
 
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(process.env.BASE_URL),
   routes,
-  scrollBehavior() {
-    return { top: 0 }
-  },
-})
+  linkActiveClass: "active",
+});
 
-export default router
+export default router;
